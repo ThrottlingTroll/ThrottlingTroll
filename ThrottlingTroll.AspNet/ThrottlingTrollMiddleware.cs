e1a9deee8e2b4a1414d58fc5677a3fb5db945483
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2024-04-20 19:00:55 +0200

    refactoring

--- ThrottlingTroll.AspNet/ThrottlingTrollMiddleware.cs
+++ ThrottlingTroll.AspNet/ThrottlingTrollMiddleware.cs
@@ -1,8 +1,4 @@
-﻿using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.Net.Http.Headers;
 using System;
 using System.Collections.Generic;
@@ -108,76 +104,5 @@ namespace ThrottlingTroll
                 }
             }
         }
-    }
-
-    /// <summary>
-    /// Extension methods for configuring ThrottlingTrollMiddleware
-    /// </summary>
-    public static class ThrottlingTrollExtensions
-    {
-        /// <summary>
-        /// Configures ThrottlingTroll ingress throttling
-        /// </summary>
-        public static IApplicationBuilder UseThrottlingTroll(this IApplicationBuilder builder, Action<ThrottlingTrollOptions> options = null)
-        {
-            var opt = new ThrottlingTrollOptions();
-
-            if (options != null)
-            {
-                options(opt);
-            }
-
-            if (opt.GetConfigFunc == null)
-            {
-                if (opt.Config == null)
-                {
-                    // Trying to read config from settings
-                    var config = builder.ApplicationServices.GetService<IConfiguration>();
-
-                    var section = config?.GetSection(ConfigSectionName);
-
-                    var throttlingTrollConfig = section?.Get<ThrottlingTrollConfig>();
-
-                    if (throttlingTrollConfig == null)
-                    {
-                        throw new InvalidOperationException($"Failed to initialize ThrottlingTroll. Settings section '{ConfigSectionName}' not found or cannot be deserialized.");
-                    }
-
-                    opt.GetConfigFunc = async () => throttlingTrollConfig;
-                }
-                else
-                {
-                    opt.GetConfigFunc = async () => opt.Config;
-                }
-            }
-
-            if (opt.Log == null)
-            {
-                var logger = builder.ApplicationServices.GetService<ILogger<ThrottlingTroll>>();
-                opt.Log = logger == null ? null : (l, s) => logger.Log(l, s);
-            }
-
-
-            if (opt.CounterStore == null)
-            {
-                opt.CounterStore = builder.GetOrCreateThrottlingTrollCounterStore();
-            }
-
-            return builder.UseMiddleware<ThrottlingTrollMiddleware>(opt);
-        }
-
-        private static ICounterStore GetOrCreateThrottlingTrollCounterStore(this IApplicationBuilder builder)
-        {
-            var counterStore = builder.ApplicationServices.GetService<ICounterStore>();
-
-            if (counterStore == null)
-            {
-                counterStore = new MemoryCacheCounterStore();
-            }
-
-            return counterStore;
-        }
-
-        private const string ConfigSectionName = "ThrottlingTrollIngress";
     }
 }
---
+++ ThrottlingTroll.AzureFunctionsAspNet/ThrottlingTrollExtensions.cs
@@ -0,0 +1,127 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+
+[assembly: InternalsVisibleTo("ThrottlingTroll.AzureFunctionsAspNet.Tests")]
+
+namespace ThrottlingTroll
+{
+    /// <summary>
+    /// Extension methods for configuring ThrottlingTrollMiddleware.
+    /// </summary>
+    public static class ThrottlingTrollExtensions
+    {
+        /// <summary>
+        /// Configures ThrottlingTroll ingress throttling
+        /// </summary>
+        public static IFunctionsWorkerApplicationBuilder UseThrottlingTroll(this IFunctionsWorkerApplicationBuilder builder, Action<ThrottlingTrollOptions> options = null)
+        {
+            return builder.UseThrottlingTroll(options == null ? null : (ctx, opt) => options(opt));
+        }
+
+        /// <summary>
+        /// Configures ThrottlingTroll ingress throttling
+        /// </summary>
+        public static IFunctionsWorkerApplicationBuilder UseThrottlingTroll(this IFunctionsWorkerApplicationBuilder builder, Action<FunctionContext, ThrottlingTrollOptions> options)
+        {
+            // Need to create a single instance, yet still allow for multiple copies of ThrottlingTrollMiddleware with different settings
+            var lockObject = new object();
+            ThrottlingTrollMiddleware middleware = null;
+
+            return builder.UseWhen
+            (
+                (FunctionContext context) =>
+                {
+                    // This middleware is only for http trigger invocations.
+                    return context
+                        .FunctionDefinition
+                        .InputBindings
+                        .Values
+                        .First(a => a.Type.EndsWith("Trigger"))
+                        .Type == "httpTrigger";
+                },
+                async (FunctionContext context, Func<Task> next) =>
+                {
+                    if (middleware == null)
+                    {
+                        lock (lockObject)
+                        {
+                            if (middleware == null)
+                            {
+                                var opt = new ThrottlingTrollOptions();
+
+                                if (options != null)
+                                {
+                                    options(context, opt);
+                                }
+
+                                middleware = CreateMiddleware(context, opt);
+                            }
+                        }
+                    }
+                    await middleware.Invoke(context, next);
+                }
+            );
+        }
+
+        private static ThrottlingTrollMiddleware CreateMiddleware(FunctionContext context, ThrottlingTrollOptions opt)
+        {
+            if (opt.GetConfigFunc == null)
+            {
+                if (opt.Config == null)
+                {
+                    // Trying to read config from settings.
+                    var config = context.InstanceServices.GetService<IConfiguration>();
+
+                    var section = config?.GetSection(ConfigSectionName);
+
+                    var throttlingTrollConfig = section?.Get<ThrottlingTrollConfig>();
+
+                    if (throttlingTrollConfig == null)
+                    {
+                        throw new InvalidOperationException($"Failed to initialize ThrottlingTroll. Settings section '{ConfigSectionName}' not found or cannot be deserialized.");
+                    }
+
+                    opt.GetConfigFunc = async () => throttlingTrollConfig;
+                }
+                else
+                {
+                    opt.GetConfigFunc = async () => opt.Config;
+                }
+            }
+
+            if (opt.Log == null)
+            {
+                var logger = context.InstanceServices.GetService<ILogger<ThrottlingTroll>>();
+                opt.Log = logger == null ? null : (l, s) => logger.Log(l, s);
+            }
+
+            if (opt.CounterStore == null)
+            {
+                opt.CounterStore = context.GetOrCreateThrottlingTrollCounterStore();
+            }
+
+            return new ThrottlingTrollMiddleware(opt);
+        }
+
+        private static ICounterStore GetOrCreateThrottlingTrollCounterStore(this FunctionContext context)
+        {
+            var counterStore = context.InstanceServices.GetService<ICounterStore>();
+
+            if (counterStore == null)
+            {
+                counterStore = new MemoryCacheCounterStore();
+            }
+
+            return counterStore;
+        }
+
+        private const string ConfigSectionName = "ThrottlingTrollIngress";
+    }
+}
