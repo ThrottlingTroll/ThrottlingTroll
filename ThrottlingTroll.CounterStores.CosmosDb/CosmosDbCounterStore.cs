4a8c81c093cc9a445c1aa2b3b22ac93d62c8b548
Konstantin Lepeshenkov <5447190+scale-tone@users.noreply.github.com>
2024-11-20 15:48:38 +0100

    initial commit

---
+++ ThrottlingTroll.CounterStores.CosmosDb/CosmosDbCounterStore.cs
@@ -0,0 +1,88 @@
+﻿using Azure;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace ThrottlingTroll.CounterStores.AzureTable
+{
+    /// <summary>
+    /// Implements Store for rate limit counters using Azure Tables / Cosmos DB for Table
+    /// </summary>
+    public class CosmosDbCounterStore : ICounterStore
+    {
+        public CosmosDbCounterStore(CosmosClient cosmosClient, string dbName, string containerName = "ThrottlingTroll")
+        {
+            this._container = cosmosClient.GetContainer(dbName, containerName);
+        }
+
+        /// <inheritdoc />
+        public Action<LogLevel, string> Log { get; set; }
+
+        /// <inheritdoc />
+        public async Task<long> GetAsync(string key, IHttpRequestProxy request)
+        {
+            try
+            {
+                var response = await this._container.ReadItemAsync<CounterEntity>(key, new PartitionKey(key));
+
+                if (response.Resource.expiresAt < DateTime.UtcNow)
+                {
+                    return 0;
+                }
+
+                return response.Resource.count;
+            }
+            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                return 0;
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task<long> IncrementAndGetAsync(string key, long cost, DateTimeOffset ttl, long maxCounterValueToSetTtl, IHttpRequestProxy request)
+        {
+            try
+            {
+                var response = await this._container.PatchItemAsync<CounterEntity>(key, new PartitionKey(key), new PatchOperation[]
+                {
+                    PatchOperation.Increment($"/count", cost)
+                });
+
+
+            }
+            catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
+            {
+                var createResponse = await this._container.CreateItemAsync(new CounterEntity
+                {
+                    id = key,
+                    count = cost,
+                    expiresAt = ttl
+                });
+
+                return 0;
+            }
+
+            return 0;
+        }
+
+        /// <inheritdoc />
+        public async Task DecrementAsync(string key, long cost, IHttpRequestProxy request)
+        {
+        }
+
+        internal class CounterEntity
+        {
+            public string id { get; set; } = default!;
+
+            public long count { get; set; }
+
+            public DateTimeOffset expiresAt { get; set; }
+        }
+
+        private readonly Container _container;
+    }
+}
---
+++ ThrottlingTroll.CounterStores.CosmosDb/README.md
@@ -0,0 +1,37 @@
+# ThrottlingTroll.CounterStores.AzureTable
+
+[ICounterStore](https://github.com/ThrottlingTroll/ThrottlingTroll/blob/main/ThrottlingTroll.Core/CounterStores/ICounterStore.cs) 
+implementation that uses Azure Tables (or Cosmos DB with Table API).
+
+Easiest to configure (only takes a Storage connection string), but not recommended for production workloads, 
+because contention around the table might get very high (remember that Azure Storage Tables only support 
+optimistic concurrency and no atomic operations).
+
+[<img alt="Nuget" src="https://img.shields.io/nuget/dt/ThrottlingTroll.CounterStores.AzureTable?label=ThrottlingTroll.CounterStores.AzureTable%20downloads">](https://www.nuget.org/packages/ThrottlingTroll.CounterStores.AzureTable)
+
+
+## How to use
+
+1. Install package from NuGet:
+    ```
+    dotnet add package ThrottlingTroll.CounterStores.AzureTable
+    ```
+
+2. EITHER put **AzureTableCounterStore** instance into your DI container:
+
+    ```
+    builder.Services.AddSingleton<ICounterStore>(
+        new AzureTableCounterStore("my-azure-storage-connection-string", "ThrottlingTrollCountersTable")
+    );
+    ```
+     OR provide an instance of it via **.UseThrottlingTroll()** method:
+
+    ```
+    app.UseThrottlingTroll(options => {
+        options.CounterStore = new AzureTableCounterStore("my-azure-storage-connection-string", "ThrottlingTrollCountersTable");
+    });
+    ```
+    
+The second parameter - table name - can be anything you like. That table will be auto-created, if not exists yet.
+
+IMPORTANT: if you use Cosmos DB, then its default consistency level should be set to either `STRONG` or `BOUNDED STALENESS`.
